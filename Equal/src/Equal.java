254e62ab84feba5aefd6ab8cbc71aa37687019fa
VAIBHAV <VAIBHAV@LENOVO>
2014-03-15 11:30:46 +0530

    --> Equal initial commit

--- Equal/src/Equal.java
+++ Equal/src/Equal.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Equal {
 
 	public static void main(String [] args ) 
@@ -8,23 +9,31 @@ public class Equal {
 		int number1;
 		int number2;
 		System.out.print("enter 1st: ");
-		number1  = input.nextInt(); // hi my name is 12
-		System.out.print("enter 2nd: ");
-		number2  =  input.nextInt();
-		
-		if (number1 == number2){
-			System.out.printf("%d == %d\n" ,number1,number2);
+		String line1  = input.nextLine(); // hi my name is 12
+		System.out.println(line1);
+		try{
+			number1 = Integer.parseInt(line1);
+			System.out.print("enter 2nd: ");
+			number2  =  input.nextInt();
+			
+			if (number1 == number2){
+				System.out.printf("%d == %d\n" ,number1,number2);
+			}
+			
+			if (number1 != number2){
+				System.out.printf( "%d != %d\n",number1,number2);
+			}
+			
+			if (number1 < number2){
+				System.out.printf("%d < %d\n",number1,number2);
+			}
+			if (number1 > number2){
+				System.out.printf("%d > %d\n",number1,number2);
+			}
+
 		}
-		
-		if (number1 != number2){
-			System.out.printf( "%d != %d\n",number1,number2);
-		}
-		
-		if (number1 < number2){
-			System.out.printf("%d < %d\n",number1,number2);
-		}
-		if (number1 > number2){
-			System.out.printf("%d > %d\n",number1,number2);
+		catch(NumberFormatException ex){
+			System.out.println("Exception "+ex);
 		}
 	}
 
